0573e531e46aa335428663f2aaa2527a4e6e97f3
fineCoderWithLove <3151128964@qq.com>
2023-12-14 22:27:09 +0800

    简单封装了orm日志和sqlserssion

---
+++ aorm/sqlsession/raw.go
@@ -0,0 +1,61 @@
+package sqlsession
+
+import (
+	"aorm/log"
+	"database/sql"
+	"strings"
+)
+
+type Session struct {
+	db      *sql.DB
+	sql     strings.Builder
+	sqlVars []interface{}
+}
+
+func New(db *sql.DB) *Session {
+	return &Session{db: db}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+// 用与拼接和链式调用
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
+
+// 非查询单返回
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+// 查询单行 对象返回
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
+
+// 查询多行 多对象返回
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
